2f682db0e3f788499cad54279417245c8a4cfbec
Andrey Gazhala <andreygazhala@gmail.com>
2023-05-09 23:02:09 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,8 +6,5 @@ const PORT = 3000;
 const server = http.createServer(routes.routesHandler);
 
 server.listen(PORT, () => {
-    const a = [{
-        a:1
-    }]
     console.log(`Server running at http://${HOST}:${PORT}/`);
 });
