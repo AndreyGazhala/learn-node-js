458d14e2d5e03c3e6a074c6de95749ef395a5f0e
Andrey Gazhala <andreygazhala@gmail.com>
2023-05-10 19:18:08 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const http = require('http');
 const routes = require('./routes_handler');
 const HOST = 'localhost';
-const PORT = 3000;
+const PORT = process.env.PORT || 3030;
 
 const server = http.createServer(routes.routesHandler);
 
 server.listen(PORT, () => {
-    console.log('HTTP SERVER STARTED');
     console.log(`Server running at http://${HOST}:${PORT}/`);
 });
