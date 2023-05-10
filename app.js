02713d001f316b39b7b9739c421316d286264961
Andrey Gazhala <andreygazhala@gmail.com>
2023-05-10 11:35:11 +0300

    added test commit

--- app.js
+++ app.js
@@ -6,5 +6,6 @@ const PORT = 3000;
 const server = http.createServer(routes.routesHandler);
 
 server.listen(PORT, () => {
+    console.log('HTTP SERVER STARTED');
     console.log(`Server running at http://${HOST}:${PORT}/`);
 });
